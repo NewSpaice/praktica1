58700fb9588bd54774502f2049ce4750a387e303
unknown <229191-31>
2023-04-10 15:20:29 +0300

    123

--- Новый текстовый документ.cpp
+++ Новый текстовый документ.cpp
@@ -6,19 +6,18 @@ void main () {
     cout << "leonov is cool";
     while (true)
     {
-<<<<<<< HEAD
+
     int i--;
     cout << "Уменьшается"<<i;
     cout<<endl;
-=======
+
     int i++;
-<<<<<<< HEAD
+
     cout << i+1;
->>>>>>> bb4a329969d8ab1b18b8ae41c1676a59bd7adfe4
-=======
+
     cout << i<<endl;
     cout << "1000-7"<<endl;
->>>>>>> f16ad2159356817b0b7680b153d5d605dda893a2
+
     }
 system("pause");
 return 0;
